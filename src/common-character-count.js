cd18c0725ba19a311a3a85b0647e083c9bd10a7e
Yana Laurynovich <ylaurynovich@godaddy.com>
2023-12-22 13:36:10 +0500

    feat: impl common char

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const mapWithCountForS1 = getMapWithCount(s1);
+  const mapWithCountForS2 = getMapWithCount(s2);
+
+  return Object.values(findIntersectionMaps(mapWithCountForS1, mapWithCountForS2)).reduce(
+    (prevValue, currentValue) => prevValue + currentValue,
+    0
+  );
+}
+
+function getMapWithCount(s) {
+  let mapWithCount = {};
+  [...s].forEach((symbol) => {
+    if (mapWithCount[symbol] !== undefined) {
+      mapWithCount[symbol]++;
+    } else {
+      mapWithCount[symbol] = 1;
+    }
+  });
+
+  return mapWithCount;
+}
+
+function findIntersectionMaps(map1, map2) {
+  let intersection = {};
+  for (const value in map1) {
+    if (map2[value] !== undefined) {
+      intersection[value] =
+        map2[value] < map1[value] ? map2[value] : map1[value];
+    }
+  }
+  return intersection;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
